4aebbbfa678074706fbeebe353e29653d87d4a46
Victorya Komarnitskaya <komarnitckaia.victoriia@mail.ru>
2024-03-10 18:34:29 +0300

    modified Main class for two numbers

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        System.out.println("Сумма введенных чисел равна: " + (firstNumber + secondNumber));
+        System.out.println("Разность введенных чисел равна: " + (firstNumber - secondNumber));
+        System.out.println("Произведение введенных чисел равно: " + (firstNumber * secondNumber));
+        System.out.println("Частное введенных чисел равно: " + ((double) firstNumber / secondNumber));
     }
 }
